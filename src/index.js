936f33d6bd56be148445a9217c64cc4a38216286
MarcosMoura <marcos_moura@live.com>
2019-06-28 20:59:34 -0300

    Mais Configurações padrão

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./config/ReactotronConfig";
+import "./config/DevToolsConfig";
+
+// pra sabermos onde colocar os imports
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, ScrollView, View, Text } from "react-native";
+
+import Post from "./Post";
