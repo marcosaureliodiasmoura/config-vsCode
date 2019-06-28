a6b095cd7f61f414a7d3bab76f911cd1e29d297c
MarcosMoura <marcos_moura@live.com>
2019-06-28 20:05:14 -0300

    primeira configuração

---
+++ init.js
@@ -0,0 +1,46 @@
+{
+  // Define o tema do VSCode
+  "workbench.colorTheme": "Dracula",
+
+  //Configura o tamanho e a família da fonte
+  "editor.fontSize": 18,
+  "editor.lineHeight": 24,
+  "editor.fontFamily": "Fira Code",
+  "editor.formatOnSave": true,
+  "editor.fontLigatures": true,
+
+  // Aplica linhas verticais para lembrar de quebrar linha em código muito grande
+  "editor.rulers": [80, 120],
+  "prettier.eslintIntegration": true,
+
+  // Aplica sinal visual na esquerda da linha selecionada
+  "editor.renderLineHighlight": "gutter",
+
+  // Aumenta a fonte do terminal
+  "terminal.integrated.fontSize": 14,
+
+  // Define os temas e os ícones do sidebar
+  "workbench.startupEditor": "newUntitledFile",
+  "editor.tabSize": 2,
+
+  "window.zoomLevel": 1,
+
+  "emmet.syntaxProfiles": {
+    "javascript": "jsx"
+  },
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  "typescript.updateImportsOnFileMove.enabled": "never",
+  "breadcrumbs.enabled": true,
+  "editor.parameterHints.enabled": false,
+  "files.associations": {
+    "*.njk": "nunjucks"
+  },
+  "terminal.integrated.rendererType": "dom",
+  "git.autofetch": true,
+  "git.enableSmartCommit": true,
+  "workbench.iconTheme": "vscode-icons",
+  "liveServer.settings.donotShowInfoMsg": true,
+  "javascript.updateImportsOnFileMove.enabled": "always"
+}
